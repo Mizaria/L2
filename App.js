e53f8b584aa7424785cd6affc0408c1fafc6e846
Su Yan Kai <23019805@myrp.edu.sg>
2024-10-25 14:31:53 +0800

    Updated Folders

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image,ScrollView } from 'react-native';
 import Movie from './components/movie';
 
 // AllMovie Component
 const AllMovie = () => {
   return (
-      <View>
+      <ScrollView>
         <Text></Text>
         <Text></Text>
           <Movie title="Doctor Sleep" year={2019} icon_name="paw" poster = {require('./img/doctor-sleep.jpg')} />
           <Movie title="Midway" year={2020} icon_name="skull" poster = {require('./img/midway.jpg')} />
-      </View>
+      </ScrollView>
   );
 };
 
